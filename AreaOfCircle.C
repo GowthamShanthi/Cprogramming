94fefb3ce9e53a8793a02159bee40ea52143b98b
GowthamShanthi <101311078+GowthamShanthi@users.noreply.github.com>
2023-01-12 19:08:31 +0530

    Add files via upload

---
+++ AreaOfCircle.C
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  float  r;
+  float area;
+  
+  
+  
+ printf("Enter the Radius");
+ scanf("%f",&r);
+ 
+ area = (22/7.0)*r*r;
+ 
+ printf("The area of the circle with radius %.2f is %.2f",r,area);
+ 
+
+     
+}

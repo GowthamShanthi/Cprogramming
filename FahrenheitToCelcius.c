330b7331797ff5c7875c4e854c78b19fcb61c59d
GowthamShanthi <101311078+GowthamShanthi@users.noreply.github.com>
2023-01-13 18:35:34 +0530

    Add files via upload
    Adding Fahrenheit to  Celsius Program

---
+++ FahrenheitToCelcius.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+ 
+ float F,C;
+ 
+ printf("Enter the Fahrenheit Value");
+ scanf("%f",&F);
+ 
+ C= ((float)5/9)*(F-32);
+ 
+ printf("The Equivalent Celscius Value of the given Fahrenheit Value %.0f is %.2f",F,C);
+ 
+
+ 
+ 
+ 
+}

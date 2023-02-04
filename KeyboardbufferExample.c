062a437fa88056609e9fff6ddaf5fbb63150a84c
GowthamShanthi <101311078+GowthamShanthi@users.noreply.github.com>
2023-02-04 14:00:22 +0530

    Add files via upload
    Adding Keyboard input programs

---
+++ KeyboardbufferExample.c
@@ -0,0 +1,24 @@
+
+int main  (){
+
+char a;
+
+char name[100];
+
+printf("Enter name with initials\n");
+
+
+a= getchar();
+gets(name);
+
+printf("The initials is %c    \n",a);
+printf("The name is %s",name);
+
+
+
+
+
+
+
+}
+
---
+++ StringExample.c
@@ -0,0 +1,47 @@
+
+
+
+
+
+int main(){
+
+
+char s[30] = "Namachivaya";
+
+
+char a;
+
+char b;
+
+printf("Enter the a and b Values");
+
+
+a= getchar();
+b = getchar();
+
+
+
+printf("The a Value is %c",a);
+
+printf("The b Value is %c",b);
+
+
+/* - \n - is a not printable character*/
+
+
+printf("The string is %s",s);
+printf("The second string is\n %s",s+3);
+printf("The Third character is \n %c",s[2]);
+
+
+
+
+
+
+
+
+
+ 
+
+}
+
